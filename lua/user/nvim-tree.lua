3dad5abd0cdd15737f2d04eb558c89dca6eb5a8c
Ninad Nerkar <kipintouch29@gmail.com>
2023-06-04 11:49:47 +0530

    Updating nvim tree with on_attach

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -4,8 +4,49 @@ local M = {
   event = "VimEnter"
 }
 
+
+local function on_attach(bufnr)
+  -- This function has been generated from your
+  --   view.mappings.list
+  --   view.mappings.custom_only
+  --   remove_keymaps
+  --
+  -- You should add this function to your configuration and set on_attach = on_attach in the nvim-tree setup call.
+  --
+  -- Although care was taken to ensure correctness and completeness, your review is required.
+  --
+  -- Please check for the following issues in auto generated content:
+  --   "Mappings removed" is as you expect
+  --   "Mappings migrated" are correct
+  --
+  -- Please see https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach for assistance in migrating.
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings. Feel free to modify or remove as you wish.
+  --
+  -- BEGIN_DEFAULT_ON_ATTACH
+  api.config.mappings.default_on_attach(bufnr)
+  -- END_DEFAULT_ON_ATTACH
+
+
+  -- Mappings migrated from view.mappings.list
+  --
+  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+
+end
+
 function M.config()
-  local tree_cb = require("nvim-tree.config").nvim_tree_callback
+  -- local tree_cb = require("nvim-tree.config").nvim_tree_callback
   require("nvim-tree").setup {
     update_focused_file = {
       enable = true,
@@ -51,14 +92,15 @@ function M.config()
     view = {
       width = 30,
       side = "left",
-      mappings = {
-        list = {
-          { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-          { key = "h",                  cb = tree_cb "close_node" },
-          { key = "v",                  cb = tree_cb "vsplit" },
-        },
-      },
+      -- mappings = {
+      --   list = {
+      --     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+      --     { key = "h",                  cb = tree_cb "close_node" },
+      --     { key = "v",                  cb = tree_cb "vsplit" },
+      --   },
+      -- },
     },
+    on_attach = on_attach
   }
 end
 
