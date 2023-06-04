6c8ec22a17bac8c25efa36dd9146fe59aae3a872
Ninad Nerkar <kipintouch29@gmail.com>
2023-06-04 11:48:16 +0530

    Disable some in-build plugins

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -11,9 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- example using a list of specs with the default options
-vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
-
 -- load lazy
 require("lazy").setup("user", {
   install = { colorscheme = { require("user.colorscheme").name } },
@@ -24,14 +21,26 @@ require("lazy").setup("user", {
   performance = {
     rtp = {
       disabled_plugins = {
-        -- "gzip", -- Plugin for editing compressed files.
-        -- "matchit", -- What is it?
-        --  "matchparen", -- Plugin for showing matching parens
-        --  "netrwPlugin", -- Handles file transfers and remote directory listing across a network
-        --  "tarPlugin", -- Plugin for browsing tar files
-        --  "tohtml", -- Converting a syntax highlighted file to HTML
+        "2html_plugin",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        -- "netrw",
+        -- "netrwPlugin",
+        -- "netrwSettings",
+        -- "netrwFileHandlers",
+        "matchit",
+        "matchparen",
+        "tohtml", -- Converting a syntax highlighted file to HTML
+        "tar",
+        "tarPlugin",
         --  "tutor", -- Teaching?
-        --  "zipPlugin", -- Handles browsing zipfiles
+        "rrhelper",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
       },
     },
   },
